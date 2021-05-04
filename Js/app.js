485c7220cb388dbbb6e112d335bf9bf7fe4b8e02
khaledABUZENAH <djkw365@gmail.com>
2021-05-04 21:41:12 +0300

    lab 7 done

--- Js/app.js
+++ Js/app.js
@@ -60,3 +60,116 @@ console.log(paris);
  lima.getCustNumPerHour();
 lima.getCookiePerHour();
  console.log(lima);
+
+
+ //render seattle
+ let theParent = document.getElementById('parent');
+ console.log(theParent);
+ 
+ let theTable=document.createElement('table');
+ theParent.appendChild(theTable);
+
+ let headerRow = document.createElement('tr');
+ theTable.appendChild(headerRow);
+
+ let th = document.createElement('th');
+headerRow.appendChild(th);
+
+
+for (let i = 0;i < time.length;i++) {
+  
+  let th2 = document.createElement('th');
+headerRow.appendChild(th2);
+th2.textContent =time[i];
+
+}
+
+
+let th3 = document.createElement("th");
+headerRow.appendChild(th3);
+th3.textcontent ="Daily";
+
+
+
+
+
+let tr2=document.createElement("tr");
+theTable.appendChild(tr2);
+tr2.textContent="Seattle";
+
+
+for (let i = 0; i < time.length; i++) {
+  
+  let td=document.createElement("td");
+  tr2.appendChild(td);
+  td.textContent=seattle.cookiePerHour[i];
+   
+  
+}
+
+
+//render tokyo
+
+let tr3=document.createElement("tr");
+theTable.appendChild(tr3);
+tr3.textContent="Tokyo";
+
+
+for (let i = 0; i < time.length; i++) {
+  
+  let td2=document.createElement("td");
+  tr3.appendChild(td2);
+  td2.textContent=tokyo.cookiePerHour[i];
+   
+  
+}
+
+
+//render dubai
+
+let tr4=document.createElement("tr");
+theTable.appendChild(tr4);
+tr4.textContent="Dubai";
+
+
+for (let i = 0; i < time.length; i++) {
+  
+  let td3=document.createElement("td");
+  tr4.appendChild(td3);
+  td3.textContent=dubai.cookiePerHour[i];
+   
+  
+}
+
+//render paris
+
+let tr5=document.createElement("tr");
+theTable.appendChild(tr5);
+tr5.textContent="Paris";
+
+
+for (let i = 0; i < time.length; i++) {
+  
+  let td4=document.createElement("td");
+  tr5.appendChild(td4);
+  td4.textContent=paris.cookiePerHour[i];
+   
+  
+}
+
+//render lima
+
+let tr6=document.createElement("tr");
+theTable.appendChild(tr6);
+tr6.textContent="Lima";
+
+
+for (let i = 0; i < time.length; i++) {
+  
+  let td5=document.createElement("td");
+  tr6.appendChild(td5);
+  td5.textContent=lima.cookiePerHour[i];
+   
+  
+}
+
