f3f9dfed9b304656bd5ed8c24596a5897cf08877
khaledABUZENAH <djkw365@gmail.com>
2021-05-04 03:13:36 +0300

    prototype edited

--- Js/app.js
+++ Js/app.js
@@ -8,26 +8,55 @@ function Shop(location, minCust,maxCust,avgCockieSold){
     this.minCust=minCust;
     this.maxCust=maxCust;
     this.avgCockieSold=avgCockieSold;
+    this.custNum=[];
+    this.cookiePerHour=[];
     
     
 }
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
  Shop.prototype.getCustNumPerHour=function(){
    for (let i = 0; i < time.length; i++) {
-        this.custNum.push(randomCostumerNum(this.minCust, this.maxCust));
+        this.custNum.push(random(this.minCust, this.maxCust));
  }
-  
-  
-  let seattle=new Shop("Seattle",23,65,6.3);
-  console.log(seattle);
-  seattle.getCustNunPerHour();
-  //let tokyo=new Shop("Tokyo",3,24,1.2);
-  //let dubai=new Shop("Dubai",11,38,3.7);
-  //let paris=new Shop("Paris",20,38,2.3);
- // let lima=new Shop("Lima",2,16,4.6);
  
+}
+
+Shop.prototype.getCookiePerHour=function() {
+   for (let i = 0; i < time.length; i++) {
+    this.cookiePerHour[i] = Math.floor(this.custNum[i]*this.avgCockieSold);
+    this.total += this.cookiePerHour[i];
+  ;
+}
+}
+
+
+let seattle=new Shop("Seattle",23,65,6.3);
+seattle.getCustNumPerHour();
+seattle.getCookiePerHour();
+console.log(seattle);
 
 
 
-  }
-  
+let tokyo=new Shop("Tokyo",3,24,1.2);
+tokyo.getCustNumPerHour();
+tokyo.getCookiePerHour();
+console.log(tokyo);
+
+let dubai=new Shop("Dubai",11,38,3.7);
+dubai.getCustNumPerHour();
+dubai.getCookiePerHour();
+console.log(dubai);
+
+let paris=new Shop("Paris",20,38,2.3);
+paris.getCustNumPerHour();
+paris.getCookiePerHour();
+console.log(paris);
+
+ let lima=new Shop("Lima",2,16,4.6);
+ lima.getCustNumPerHour();
+lima.getCookiePerHour();
+ console.log(lima);
